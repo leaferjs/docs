391c9029e89571c50d0d5c4edd3e73418180de6d
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 01:36:20 +0800

    New translations pen.ts (English)

---
+++ en/code/property/path/pen.ts
@@ -0,0 +1,13 @@
+// #Rect 转路径优先模式 [使用 pen 绘制 (Leafer)]
+import { Leafer, Rect } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const rect = new Rect({
+  width: 100,
+  height: 100,
+  fill: '#32cd79'
+});
+leafer.add(rect);
+rect.windingRule = 'evenodd'; // [!code hl:2] 
+rect.pen.roundRect(0, 0, 100, 100, 30).drawArc(50, 50, 25);
