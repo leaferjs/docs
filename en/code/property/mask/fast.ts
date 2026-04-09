be45e902318a053d23c477ea944037bb51ea5da4
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 01:35:39 +0800

    New translations fast.ts (English)

---
+++ en/code/property/mask/fast.ts
@@ -0,0 +1,54 @@
+// #遮罩功能 [快速设置遮罩 (Leafer)]
+import { Leafer, Group, Ellipse, Image, UI } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const group = new Group({
+  x: 100,
+  y: 100,
+  draggable: true
+});
+const image = new Image({
+  width: 100,
+  height: 100,
+  url: '/image/leafer.jpg'
+});
+addMask(group, new Ellipse({
+  // [!code hl:7]
+  width: 100,
+  height: 100,
+  innerRadius: 0.5,
+  fill: 'black'
+}));
+leafer.add(group);
+group.add(image);
+console.log(getMask(group) // ellipse
+);
+
+// 自定义函数
+
+function addMask(group: Group, maskChild: UI) {
+  if (group.children.some(item => item.mask)) removeMask(group);
+  maskChild.mask = true;
+  group.addAt(maskChild, 0);
+}
+function getMask(group: Group): UI {
+  return (group.children.find(item => item.mask) as UI);
+}
+function removeMask(group: Group, maskChild?: UI): void {
+  if (maskChild) {
+    maskChild.mask = false;
+    group.remove(maskChild);
+  } else {
+    const {
+      children
+    } = group;
+    for (let i = 0, len = children.length; i < len; i++) {
+      maskChild = (children[i] as UI);
+      if (maskChild.mask) {
+        removeMask(group, maskChild);
+        len--, i--;
+      }
+    }
+  }
+}
