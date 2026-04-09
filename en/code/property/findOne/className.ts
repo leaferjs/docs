7e33ca95278faeadd64e9413d32a0ac18068c936
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 01:34:51 +0800

    New translations classname.ts (English)

---
+++ en/code/property/findOne/className.ts
@@ -0,0 +1,25 @@
+// #查找单个元素 [通过 className 查找 (Leafer)]
+import { Leafer, Rect } from 'leafer-ui';
+import '@leafer-in/find'; // 导入查找元素插件 // [!code hl] 
+
+const leafer = new Leafer({
+  view: window
+});
+const rect1 = new Rect({
+  className: 'menu',
+  fill: '#32cd79'
+});
+const rect2 = new Rect({
+  className: 'menu',
+  fill: '#32cd79',
+  x: 150
+});
+const rect3 = new Rect({
+  fill: '#32cd79',
+  x: 300
+});
+leafer.add(rect1);
+leafer.add(rect2);
+leafer.add(rect3);
+console.log(leafer.findOne('.menu') // [!code hl] // rect1
+);
