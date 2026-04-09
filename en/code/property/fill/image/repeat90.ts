8fa0b3f090bc8eae888fb276b40e4f8f23083cde
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 01:34:08 +0800

    New translations repeat90.ts (English)

---
+++ en/code/property/fill/image/repeat90.ts
@@ -0,0 +1,18 @@
+// #图案填充 [repeat 平铺模式旋转 90 度 (Leafer)]
+import { Leafer, Rect } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const rect = new Rect({
+  width: 100,
+  height: 100,
+  fill: {
+    // [!code hl:7]
+    type: 'image',
+    url: '/image/leafer.jpg',
+    mode: 'repeat',
+    scale: 0.2,
+    rotation: 90
+  }
+});
+leafer.add(rect);
