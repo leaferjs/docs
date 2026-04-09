6243638b7b42f8c30424c910822a6cfbc6d9f55f
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 01:35:13 +0800

    New translations hitchildren.ts (English)

---
+++ en/code/property/hit/hitChildren.ts
@@ -0,0 +1,27 @@
+// #停用子元素交互事件 (Leafer)
+import { Leafer, Group, Rect, Ellipse } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const group = new Group({
+  draggable: true,
+  children: [new Rect({
+    width: 100,
+    height: 100,
+    fill: '#32cd79',
+    draggable: true
+  }), new Ellipse({
+    x: 50,
+    y: 50,
+    width: 100,
+    height: 100,
+    innerRadius: 0.5,
+    fill: "#FEB027",
+    draggable: true
+  })]
+});
+leafer.add(group);
+setTimeout(() => {
+  // 停用子元素交互事件，子元素将不能被单独拖拽
+  group.hitChildren = false; // [!code hl]
+}, 1000);
