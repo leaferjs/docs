f453ede27896e0a7ac677c105af6e27fb5c036ce
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 01:33:39 +0800

    New translations oncanvas.ts (English)

---
+++ en/code/property/export/onCanvas.ts
@@ -0,0 +1,28 @@
+// #导出图片 [绘制水印 (Leafer)]
+import { Leafer, Rect } from 'leafer-ui';
+import '@leafer-in/export'; // 引入导出元素插件 // [!code hl] 
+
+const leafer = new Leafer({
+  view: window
+});
+const rect = Rect.one({
+  fill: '#32cd79'
+}, 100, 100);
+leafer.add(rect);
+rect.export('test.png', {
+  pixelRatio: 2,
+  onCanvas(canvas) {
+    // 通过onCanvas钩子函数绘制水印 // [!code hl:10]
+    const {
+      context,
+      // CanvasRenderingContext2D，原生canvas上下文对象
+      pixelRatio,
+      // 像素比
+      width,
+      // 逻辑宽度， 获取实际像素宽度请使用 pixelWidth
+      height // 逻辑高度， 获取实际像素高度请使用 pixelHeight
+    } = canvas;
+    context.scale(pixelRatio, pixelRatio); // 抹平像素比差异
+    context.fillText('绘制水印', width - 60, height - 20);
+  }
+});
