d4d5c147998669988df002402eb71c120ae7ef06
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 01:37:30 +0800

    New translations a.ts (English)

---
+++ en/code/property/stroke/image/a.ts
@@ -0,0 +1,16 @@
+// #图案描边 [默认 cover 覆盖模式 (Leafer)]
+import { Leafer, Rect } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const rect = new Rect({
+  width: 100,
+  height: 100,
+  stroke: {
+    // [!code hl:5]
+    type: 'image',
+    url: '/image/leafer.jpg'
+    // mode: 'cover' // 默认模式，相当于 CSS 的 background-size: cover
+  }
+});
+leafer.add(rect);
