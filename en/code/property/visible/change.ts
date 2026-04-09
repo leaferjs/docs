a60ca6212bed767ac56a77ecab3d708557449eea
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 01:37:49 +0800

    New translations change.ts (English)

---
+++ en/code/property/visible/change.ts
@@ -0,0 +1,15 @@
+// #隐藏元素 (Leafer)
+import { Leafer, Rect } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const rect = Rect.one({
+  fill: '#32cd79'
+}, 100, 100);
+leafer.add(rect);
+setTimeout(() => {
+  // #region main
+  // 隐藏元素
+  rect.visible = false;
+  // #endregion main
+}, 1000);
