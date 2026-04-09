2878bb997bc855203595726e1453fddbfca8f9cb
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 01:31:22 +0800

    New translations color.ts (English)

---
+++ en/code/property/animation/color.ts
@@ -0,0 +1,24 @@
+// #动画样式 [颜色过渡 (Leafer)]
+import { Leafer, Rect } from 'leafer-ui';
+import '@leafer-in/animate'; // 导入动画插件 // [!code hl] 
+
+const leafer = new Leafer({
+  view: window
+});
+const rect = new Rect({
+  y: 100,
+  cornerRadius: 50,
+  fill: '#32cd79',
+  animation: {
+    // [!code hl:6]
+    style: {
+      x: 500,
+      cornerRadius: 0,
+      fill: '#ffcd00'
+    },
+    // style keyframe
+    duration: 1,
+    swing: true // 摇摆循环播放
+  }
+});
+leafer.add(rect);
