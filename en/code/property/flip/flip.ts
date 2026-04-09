10138ffa6e63537b77496d056a683e4432a8b7fe
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 01:34:59 +0800

    New translations flip.ts (English)

---
+++ en/code/property/flip/flip.ts
@@ -0,0 +1,13 @@
+// #通过 flip() 镜像元素 [无动画过渡 (Leafer)]
+import { Leafer, Rect } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const rect = Rect.one({
+  fill: '#32cd79'
+});
+leafer.add(rect);
+setTimeout(() => {
+  // 按 X 轴镜像元素
+  rect.flip('x'); // [!code hl]
+}, 1000);
