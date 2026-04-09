cd521cbe49cdc7b58a6b974d2d6dd35940815da8
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 01:38:09 +0800

    New translations config.ts (English)

---
+++ en/config.ts
@@ -0,0 +1,31 @@
+import { defineAdditionalConfig } from 'vitepress';
+import { apiSidebar } from './config/sidebar/api';
+import { docSidebar } from './config/sidebar/doc';
+import { nav } from './config/nav';
+import { search } from './config/search';
+export default defineAdditionalConfig({
+  themeConfig: {
+    nav,
+    sidebar: {
+      '/': {
+        base: '/',
+        items: docSidebar
+      },
+      '/api/': {
+        base: '/',
+        items: apiSidebar
+      }
+    },
+    search,
+    outlineTitle: '导航',
+    lastUpdatedText: '更新于',
+    docFooter: {
+      prev: '上一页',
+      next: '下一页'
+    },
+    editLink: {
+      pattern: 'https://github.com/leaferjs/docs/edit/main/:path',
+      text: '在 GitHub 上编辑此页'
+    }
+  }
+});
