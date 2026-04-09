d50cbb2d3a8916d17b753e908adf78a9dd969ee7
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 01:36:36 +0800

    New translations rotate.ts (English)

---
+++ en/code/property/rotate/rotate.ts
@@ -0,0 +1,13 @@
+// #通过 rotateOf() 旋转元素 [无动画过渡 (Leafer)]
+import { Leafer, Rect } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const rect = Rect.one({
+  fill: '#32cd79'
+}, 100, 100);
+leafer.add(rect);
+setTimeout(() => {
+  // 围绕中心点继续旋转 45度
+  rect.rotateOf('center', 45); // [!code hl]
+}, 1000);
