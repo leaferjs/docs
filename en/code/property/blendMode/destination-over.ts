63b752d178c76a2d74bc79827e0eb5324fcf983b
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 01:32:23 +0800

    New translations destination-over.ts (English)

---
+++ en/code/property/blendMode/destination-over.ts
@@ -0,0 +1,18 @@
+// #混合模式 [destination-over 在现有画布内容的后面绘制新的图形 (Leafer)]
+import { Leafer, Rect } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const rect1 = Rect.one({
+  fill: '#FF4B4B',
+  cornerRadius: 20
+}, 100, 100);
+const rect2 = Rect.one({
+  fill: '#32cd79',
+  cornerRadius: 50
+}, 150, 150);
+leafer.add([rect1, rect2]);
+setTimeout(() => {
+  // 设置混合模式
+  rect2.blendMode = 'destination-over'; // [!code hl]
+}, 1000);
