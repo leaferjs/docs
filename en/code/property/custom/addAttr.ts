7865c25df821fc6cfb88d31b90fe3e834467b78b
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 01:33:00 +0800

    New translations addattr.ts (English)

---
+++ en/code/property/custom/addAttr.ts
@@ -0,0 +1,21 @@
+// #新增元素属性 [为文本新增一个 float 属性 (Leafer)]
+import { Leafer, Text } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+Text.addAttr('float', 'left'); //  [!code hl]
+
+// default float
+
+const text = new Text({
+  text: 'Welcome to LeaferJS'
+});
+leafer.add(text);
+console.log((text as any).float); // left
+
+// set float
+
+const text2 = new Text(({
+  float: 'right'
+} as any));
+console.log((text2 as any).float); // right
