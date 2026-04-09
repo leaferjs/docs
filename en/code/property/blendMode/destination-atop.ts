077a6fb8357aef35c59703e5464a005313e7d3b9
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 01:32:19 +0800

    New translations destination-atop.ts (English)

---
+++ en/code/property/blendMode/destination-atop.ts
@@ -0,0 +1,18 @@
+// #混合模式 [destination-atop 仅保留现有画布内容和新形状重叠的部分，新形状是在现有画布内容的后面绘制的 (Leafer)]
+import { Leafer, Rect } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const rect1 = Rect.one({
+  fill: '#FF4B4B',
+  cornerRadius: 20
+}, 100, 100);
+const rect2 = Rect.one({
+  fill: '#32cd79',
+  cornerRadius: 50
+}, 150, 150);
+leafer.add([rect1, rect2]);
+setTimeout(() => {
+  // 设置混合模式
+  rect2.blendMode = 'destination-atop'; // [!code hl]
+}, 1000);
