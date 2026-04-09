65ef788288d0a0293f3c136ef3334dd47eb65fa6
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 01:37:50 +0800

    New translations none.ts (English)

---
+++ en/code/property/visible/none.ts
@@ -0,0 +1,13 @@
+// #隐藏元素，且不占空间 (Leafer)
+import { Leafer, Rect } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const rect = Rect.one({
+  fill: '#32cd79'
+}, 100, 100);
+leafer.add(rect);
+setTimeout(() => {
+  // 隐藏元素，且不占空间
+  rect.visible = 0; // [!code hl]
+}, 1000);
