0dd271fa07c2e75f228e3b51e3b2a06f888bd8a0
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 10:15:41 +0800

    New translations cover90.ts (English)

---
+++ en/code/property/fill/image/cover90.ts
@@ -0,0 +1,16 @@
+// #图案填充 [cover 覆盖模式旋转 90 度 (Leafer)]
+import { Leafer, Rect } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const rect = new Rect({
+  width: 100,
+  height: 100,
+  fill: {
+    // [!code hl:5]
+    type: 'image',
+    url: '/image/leafer.jpg',
+    rotation: 90
+  }
+});
+leafer.add(rect);
