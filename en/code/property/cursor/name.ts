190aaa8a23e9f24c44e7a639f6bc490aa03af84b
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 10:14:45 +0800

    New translations name.ts (English)

---
+++ en/code/property/cursor/name.ts
@@ -0,0 +1,14 @@
+// #光标样式 [设置光标 (Leafer)]
+import { Leafer, Polygon } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const polygon = new Polygon({
+  width: 100,
+  height: 100,
+  points: [10, 90, 10, 10, 50, 70, 90, 10, 90, 90, 90, 90, 10, 90],
+  curve: true,
+  fill: '#32cd79',
+  cursor: 'no-drop' // [!code hl]
+});
+leafer.add(polygon);
