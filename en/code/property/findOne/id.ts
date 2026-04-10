845d085729d46acb180d61d5dac784d9b6f6b69d
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 10:16:23 +0800

    New translations id.ts (English)

---
+++ en/code/property/findOne/id.ts
@@ -0,0 +1,19 @@
+// #查找单个元素 [通过 id 查找 (Leafer)]
+import { Leafer, Rect } from 'leafer-ui';
+import '@leafer-in/find'; // 导入查找元素插件 // [!code hl] 
+
+const leafer = new Leafer({
+  view: window
+});
+const rect1 = new Rect({
+  id: 'block',
+  fill: '#32cd79'
+});
+const rect2 = new Rect({
+  fill: '#32cd79'
+});
+leafer.add(rect1);
+leafer.add(rect2);
+console.log(leafer.findOne('#block') // [!code hl:2] // rect1
+// = leafer.findId('block') 
+);
