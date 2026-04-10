5a7cbe790e3e0cf6e2752996ed31580dee62c86b
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 10:13:34 +0800

    New translations delete.ts (English)

---
+++ en/code/property/animation/delete.ts
@@ -0,0 +1,19 @@
+// #动画样式 [删除文本动画 (Leafer)]
+import { Leafer, Text } from 'leafer-ui';
+import '@leafer-in/animate'; // 导入动画插件 // [!code hl] 
+
+const leafer = new Leafer({
+  view: window
+});
+const text = new Text({
+  fill: '#32cd79',
+  text: 'Welcome to LeaferJS',
+  animation: {
+    // [!code hl:4]
+    style: {
+      text: ''
+    },
+    duration: 2
+  }
+});
+leafer.add(text);
