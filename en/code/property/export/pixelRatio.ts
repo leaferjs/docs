a056ce59084d322297b38e5d6dbe67fac12c581f
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 10:15:20 +0800

    New translations pixelratio.ts (English)

---
+++ en/code/property/export/pixelRatio.ts
@@ -0,0 +1,16 @@
+// #导出图片 [导出高清图 (Leafer)]
+import { Leafer, Rect } from 'leafer-ui';
+import '@leafer-in/export'; // 引入导出元素插件 // [!code hl] 
+
+const leafer = new Leafer({
+  view: window
+});
+const rect = Rect.one({
+  fill: '#32cd79'
+}, 100, 100);
+leafer.add(rect);
+rect.export('HD.png', {
+  pixelRatio: 2
+}); // 导出2倍高清图 [!code hl:3]
+
+// const result = await rect.export('HD.png', { pixelRatio: 2 }}
