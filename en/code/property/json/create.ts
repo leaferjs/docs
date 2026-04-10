cbdd277d30e06c9bc4b984741ef85477c942c9b3
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 10:16:48 +0800

    New translations create.ts (English)

---
+++ en/code/property/json/create.ts
@@ -0,0 +1,21 @@
+// #创建元素 [使用 JSON (Leafer)]
+import { Leafer } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const json = {
+  "tag": 'Group',
+  "x": 20,
+  "y": 20,
+  "children": [{
+    "tag": "Rect",
+    "x": 100,
+    "y": 100,
+    "width": 100,
+    "height": 100,
+    "fill": "#32cd79",
+    "draggable": true
+  }]
+}; // [!code hl:3]
+
+leafer.add(json);
