48a4780e97e53d00dd197038bc25f90b4c5575c9
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 10:16:49 +0800

    New translations export.ts (English)

---
+++ en/code/property/json/export.ts
@@ -0,0 +1,17 @@
+// #导出 JSON (Leafer)
+import { Leafer, Rect } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const rect = new Rect({
+  x: 100,
+  y: 100,
+  width: 100,
+  height: 100,
+  fill: '#32cd79',
+  draggable: true
+});
+leafer.add(rect);
+const json = leafer.toJSON(); // [!code hl:3]
+
+console.log(json); // {"tag":"Leafer","width":1273,"height":877,"pixelRatio":2,"hittable":true,"children":[{"tag":"Rect","x":100,"y":100,"width":100,"height":100,"fill":"#32cd79","draggable":true}]}
