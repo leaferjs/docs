24fb92093bc446e93b6c7e675465a8579ea1652f
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 10:09:00 +0800

    New translations index.js (English)

---
+++ en/code/nodejs/skia/index.js
@@ -0,0 +1,29 @@
+const { Leafer, Rect, useCanvas } = require('@leafer-ui/node');
+const skia = require('skia-canvas');
+const http = require('http');
+useCanvas('skia', skia); // must
+
+http.createServer(function (req, res) {
+    const leafer = new Leafer({
+        width: 800,
+        height: 600,
+    });
+    leafer.add(
+        Rect.one(
+            {
+                fill: '#32cd79',
+            },
+            100,
+            100
+        )
+    );
+    leafer.export('png').then(function (result) {
+        res.writeHead(200, {
+            'Content-Type': 'text/html',
+        });
+        res.write(`<img src="${result.data}" />`);
+        res.end();
+    });
+}).listen(3000, function () {
+    console.log('[36m%s[0m', 'server is running at http://localhost:3000');
+});
