379445a5a1172a3f33c75f0e98f096eb83294caa
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 10:16:00 +0800

    New translations from.ts (English)

---
+++ en/code/property/fill/radial/from.ts
@@ -0,0 +1,26 @@
+// #径向渐变填充 [控制方向 (Leafer)]
+import { Leafer, Rect } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const rect = new Rect({
+  width: 100,
+  height: 100,
+  fill: {
+    // [!code hl:6]
+    type: 'radial',
+    // 从左上角 -> 右下角呈 45 度绘制的渐变
+    from: {
+      type: 'percent',
+      x: 0.2,
+      y: 0.2
+    },
+    to: {
+      type: 'percent',
+      x: 0.8,
+      y: 0.8
+    },
+    stops: ['#FF4B4B', '#FEB027']
+  }
+});
+leafer.add(rect);
