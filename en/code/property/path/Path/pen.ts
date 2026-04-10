02e109af2a8cd9d5333fa0197e774f772c703660
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 10:17:28 +0800

    New translations pen.ts (English)

---
+++ en/code/property/path/Path/pen.ts
@@ -0,0 +1,11 @@
+// #创建 Path [使用 pen 绘制 (Leafer)]
+import { Leafer, Path } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const path = new Path({
+  fill: '#32cd79'
+});
+leafer.add(path);
+path.windingRule = 'evenodd'; // [!code hl:2] 
+path.pen.roundRect(0, 0, 100, 100, 30).drawArc(50, 50, 25);
