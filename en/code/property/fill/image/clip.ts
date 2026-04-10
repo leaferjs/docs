bc3055c14293f06c37af52e3a6485dd4dac9184c
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 10:15:39 +0800

    New translations clip.ts (English)

---
+++ en/code/property/fill/image/clip.ts
@@ -0,0 +1,25 @@
+// #图案填充 [clip 裁剪模式 (Leafer)]
+import { Leafer, Rect } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const rect = new Rect({
+  width: 100,
+  height: 100,
+  fill: {
+    // [!code hl:7]
+    type: 'image',
+    url: '/image/leafer.jpg',
+    mode: 'clip',
+    offset: {
+      x: -40,
+      y: -90
+    },
+    scale: {
+      x: 1.1,
+      y: 1.1
+    },
+    rotation: 20
+  }
+});
+leafer.add(rect);
