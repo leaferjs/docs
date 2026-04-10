602d956d345d8542329a144c6c94f1879cb28aed
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 10:14:54 +0800

    New translations changeattr2.ts (English)

---
+++ en/code/property/custom/changeAttr2.ts
@@ -0,0 +1,16 @@
+// #修改元素属性 [修改文本默认填充色为可变颜色 (Leafer)]
+import { Leafer, Text } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+Text.changeAttr('fill', (text: Text) => {
+  return text.width === 50 ? 'blue' : 'red';
+}); //  [!code hl]
+
+const text = new Text({
+  text: 'Welcome to LeaferJS'
+});
+leafer.add(text);
+setTimeout(() => {
+  text.width = 50;
+}, 1000);
