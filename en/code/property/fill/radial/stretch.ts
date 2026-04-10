dd3e57d0853a904e87bc29205ce968aca2ea635a
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 10:16:02 +0800

    New translations stretch.ts (English)

---
+++ en/code/property/fill/radial/stretch.ts
@@ -0,0 +1,24 @@
+// #径向渐变填充 [拉伸渐变 (Leafer)]
+import { Leafer, Rect } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const rect = new Rect({
+  width: 100,
+  height: 100,
+  fill: {
+    // [!code hl:6]
+    type: "radial",
+    // 从中心 -> 右下角 呈 45 度, 且拉伸比例为 0.5 绘制的渐变
+    to: 'bottom-right',
+    stretch: 0.5,
+    stops: [{
+      offset: 0,
+      color: '#FF4A2C'
+    }, {
+      offset: 1,
+      color: '#FEB027'
+    }]
+  }
+});
+leafer.add(rect);
