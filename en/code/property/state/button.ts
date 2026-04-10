9f66f69c209d4d32569a6a6fc62e234fee52a87b
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 10:18:11 +0800

    New translations button.ts (English)

---
+++ en/code/property/state/button.ts
@@ -0,0 +1,40 @@
+// #交互状态 [同步 hover 状态的按钮 (Leafer)]
+import { Leafer, Box } from 'leafer-ui';
+import '@leafer-in/state'; // 导入交互状态插件 // [!code hl] 
+
+const leafer = new Leafer({
+  view: window,
+  fill: '#333'
+});
+const box = new Box({
+  x: 100,
+  y: 100,
+  fill: '#FEB027',
+  cornerRadius: 5,
+  button: true,
+  // 标记为按钮，子元素 Text 将自动同步交互状态 // [!code hl:10]
+  hoverStyle: {
+    fill: '#32cd79'
+  },
+  // 鼠标hover状态
+  pressStyle: {
+    fill: '#FF4B4B'
+  },
+  // 鼠标按下状态
+
+  children: [{
+    tag: 'Text',
+    text: 'Button',
+    fontSize: 16,
+    padding: [10, 20],
+    fill: 'black',
+    hoverStyle: {
+      fill: 'white'
+    },
+    // 鼠标在 button 上hover的状态  // [!code hl:1]
+    pressStyle: {
+      fontWeight: 'bold'
+    } // 鼠标在 button 上按下的状态
+  }]
+});
+leafer.add(box);
