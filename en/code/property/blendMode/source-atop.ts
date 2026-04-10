86fc741403e767c076457d274d09175c9b0b2eae
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-10 10:14:30 +0800

    New translations source-atop.ts (English)

---
+++ en/code/property/blendMode/source-atop.ts
@@ -0,0 +1,18 @@
+// #混合模式 [source-atop 只在与现有画布内容重叠的地方绘制新图形 (Leafer)]
+import { Leafer, Rect } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const rect1 = Rect.one({
+  fill: '#FF4B4B',
+  cornerRadius: 20
+}, 100, 100);
+const rect2 = Rect.one({
+  fill: '#32cd79',
+  cornerRadius: 50
+}, 150, 150);
+leafer.add([rect1, rect2]);
+setTimeout(() => {
+  // 设置混合模式
+  rect2.blendMode = 'source-atop'; // [!code hl]
+}, 1000);
