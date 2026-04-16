48de3d06edb06e08e8c45a4985def771d967fb40
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-16 09:31:16 +0800

    New translations wait.ts (English)

---
+++ en/code/property/leafer/wait.ts
@@ -0,0 +1,13 @@
+// #等待元素被添加到引擎中时，执行回调 (Leafer)
+import { Leafer, Rect } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const rect = new Rect({
+  fill: '#32cd79'
+});
+rect.waitLeafer(() => {
+  // [!code hl:3]
+  rect.draggable = true;
+});
+leafer.add(rect);
