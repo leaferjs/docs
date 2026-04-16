e5f0190012bc24a9e740f3a72c6c3283bf840d64
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-16 09:28:14 +0800

    New translations right.ts (English)

---
+++ en/code/property/around/right.ts
@@ -0,0 +1,21 @@
+// #around 属性 [around 坐标点 (50,50) 在矩形的右下角 (Leafer)]
+import { Leafer, Rect, Frame } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const rect = new Rect({
+  x: 50,
+  y: 50,
+  width: 50,
+  height: 50,
+  around: 'bottom-right',
+  // [!code hl]
+  fill: '#4DCB71',
+  draggable: true
+});
+leafer.add(new Frame({
+  width: 100,
+  height: 100,
+  fill: '#FF4A2C',
+  children: [rect]
+}));
