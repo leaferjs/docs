5b29490b941500796735eba28bce6ad318bcbedf
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-16 09:27:54 +0800

    New translations rect.ts (English)

---
+++ en/code/property/animate/rect.ts
@@ -0,0 +1,25 @@
+// #动画方法 [摇摆动画 (Leafer)]
+import { Leafer, Rect } from 'leafer-ui';
+import '@leafer-in/animate'; // 导入动画插件 // [!code hl] 
+
+const leafer = new Leafer({
+  view: window
+});
+const rect = new Rect({
+  y: 100,
+  cornerRadius: 50,
+  fill: '#32cd79'
+});
+leafer.add(rect);
+rect.animate(
+// [!code hl:7]
+{
+  x: 500,
+  cornerRadius: 0
+},
+// style keyframe
+{
+  duration: 1,
+  swing: true // 摇摆循环播放
+} // options
+);
