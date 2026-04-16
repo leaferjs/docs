3a35be3b49a5c21ec5526d78408dc1823ddf3134
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-16 09:32:21 +0800

    New translations box.ts (English)

---
+++ en/code/property/shadow/box.ts
@@ -0,0 +1,20 @@
+// #外阴影 [box-shadow (Leafer)]
+import { Leafer, Rect } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const rect = new Rect({
+  width: 100,
+  height: 100,
+  cornerRadius: 30,
+  fill: 'rgba(50,205,121,0.7)',
+  shadow: {
+    // [!code hl:7]
+    x: 10,
+    y: -10,
+    blur: 20,
+    color: '#FF0000AA',
+    box: true // box-shadow
+  }
+});
+leafer.add(rect);
