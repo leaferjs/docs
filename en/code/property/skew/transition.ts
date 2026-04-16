450a052a5812a1aff882e57e7b73b26fd7632b2c
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-16 09:32:25 +0800

    New translations transition.ts (English)

---
+++ en/code/property/skew/transition.ts
@@ -0,0 +1,15 @@
+// #通过 skewOf() 倾斜元素 [有动画过渡 (Leafer)]
+import { Leafer, Rect } from 'leafer-ui';
+import '@leafer-in/animate'; // 导入动画插件 // [!code hl] 
+
+const leafer = new Leafer({
+  view: window
+});
+const rect = Rect.one({
+  fill: '#32cd79'
+}, 100, 100);
+leafer.add(rect);
+setTimeout(() => {
+  // 围绕中心继续倾斜X轴 45度
+  rect.rotateOf('center', 45, true); // [!code hl]
+}, 1000);
