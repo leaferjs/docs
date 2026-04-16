42623359786d0fc813a135697d6913d8e65e1a9e
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-16 09:31:53 +0800

    New translations path.ts (English)

---
+++ en/code/property/path/path.ts
@@ -0,0 +1,14 @@
+// #Rect 转路径优先模式 [使用 path 属性 (Leafer)]
+import { Leafer, Rect } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const rect = new Rect({
+  width: 100,
+  height: 100,
+  windingRule: 'evenodd',
+  // [!code hl:2] 
+  path: 'X0 0 100 100 30M75 50P50 50 25',
+  fill: '#32cd79'
+});
+leafer.add(rect);
