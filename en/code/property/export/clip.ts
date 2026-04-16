c08c38c05339b2c2e7437fd5d9cced5f07149050
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-16 09:29:44 +0800

    New translations clip.ts (English)

---
+++ en/code/property/export/clip.ts
@@ -0,0 +1,22 @@
+// #导出图片 [裁剪元素 (Leafer)]
+import { Leafer, Ellipse } from 'leafer-ui';
+import '@leafer-in/export'; // 引入导出元素插件 // [!code hl] 
+
+const leafer = new Leafer({
+  view: window
+});
+const ellipse = new Ellipse({
+  width: 100,
+  height: 100,
+  innerRadius: 0.5,
+  fill: "#32cd79"
+});
+leafer.add(ellipse);
+ellipse.export('clip.jpg', {
+  clip: {
+    x: 50,
+    y: 50,
+    width: 50,
+    height: 50
+  } // 对导出元素进行裁剪 // [!code hl]
+});
