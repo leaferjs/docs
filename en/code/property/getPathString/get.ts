7d41d7b3f624fbf74fa10f45035b35c124462d48
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-16 09:30:57 +0800

    New translations get.ts (English)

---
+++ en/code/property/getPathString/get.ts
@@ -0,0 +1,17 @@
+// #获取元素的字符串路径 (Leafer)
+import { Leafer, Ellipse } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const ellipse = new Ellipse({
+  width: 100,
+  height: 100,
+  startAngle: -60,
+  endAngle: 180,
+  innerRadius: 0.5,
+  fill: "#32cd79"
+});
+leafer.add(ellipse);
+
+// 打印 svg 路径字符串
+console.log(ellipse.getPathString(true)); // [!code hl]
