85fe9f977d57765cf702c7e2ae4b501c2f03d102
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-16 09:31:43 +0800

    New translations rotate.ts (English)

---
+++ en/code/property/origin/rotate.ts
@@ -0,0 +1,22 @@
+// #原点 [围绕原点旋转 45 度 (Leafer)]
+import { Leafer, Rect, Frame } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const rect = new Rect({
+  x: 25,
+  y: 25,
+  width: 50,
+  height: 50,
+  origin: 'center',
+  // [!code hl:2]
+  rotation: 45,
+  draggable: true,
+  fill: '#4DCB71'
+});
+leafer.add(new Frame({
+  width: 100,
+  height: 100,
+  fill: '#FF4A2C',
+  children: [rect]
+}));
