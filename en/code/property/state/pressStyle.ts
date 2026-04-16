7fc31a405d74df5bbca3f63552b0c4ae5b183ad3
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-16 09:32:38 +0800

    New translations pressstyle.ts (English)

---
+++ en/code/property/state/pressStyle.ts
@@ -0,0 +1,18 @@
+// #光标按下时的交互样式 (Leafer)
+import { Leafer, Rect } from 'leafer-ui';
+import '@leafer-in/state'; // 导入交互状态插件 // [!code hl] 
+
+const leafer = new Leafer({
+  view: window
+});
+const rect = new Rect({
+  width: 100,
+  height: 100,
+  fill: 'rgba(50,205,121, 0.7)',
+  cornerRadius: 30,
+  pressStyle: {
+    // [!code hl:3] // press 样式
+    fill: 'rgba(50,205,121, 1)'
+  }
+});
+leafer.add(rect);
