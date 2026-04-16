9d5e3109423c7cc4d7254ab37ff7ae15a6479817
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-16 09:30:50 +0800

    New translations tag.ts (English)

---
+++ en/code/property/findOne/tag.ts
@@ -0,0 +1,23 @@
+// #查找单个元素 [通过 tag 查找 (Leafer)]
+import { Leafer, Rect, Ellipse } from 'leafer-ui';
+import '@leafer-in/find'; // 导入查找元素插件 // [!code hl] 
+
+const leafer = new Leafer({
+  view: window
+});
+const rect1 = new Rect({
+  fill: '#32cd79'
+});
+const rect2 = new Rect({
+  fill: '#32cd79',
+  x: 150
+});
+const ellipse = new Ellipse({
+  fill: '#32cd79',
+  x: 300
+});
+leafer.add(rect1);
+leafer.add(rect2);
+leafer.add(ellipse);
+console.log(leafer.findOne('Rect') // [!code hl] // rect1
+);
