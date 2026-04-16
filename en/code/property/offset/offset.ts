e717447d7453a73140f4a3131408213059598f6d
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-16 09:31:34 +0800

    New translations offset.ts (English)

---
+++ en/code/property/offset/offset.ts
@@ -0,0 +1,16 @@
+// #偏移元素 (Leafer)
+import { Leafer, Rect } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const rect = Rect.one({
+  fill: '#32cd79'
+}, 100, 100);
+leafer.add(rect);
+setTimeout(() => {
+  // 沿 X/Y 偏移 100 像素
+  rect.set({
+    offsetX: 100,
+    offsetY: 100
+  }); // [!code hl]
+}, 1000);
