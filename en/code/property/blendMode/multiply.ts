1d537969c91ac274104732bdbda50d8386c421b9
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-16 09:28:54 +0800

    New translations multiply.ts (English)

---
+++ en/code/property/blendMode/multiply.ts
@@ -0,0 +1,18 @@
+// #混合模式 [multiply 正片叠底 (Leafer)]
+import { Leafer, Rect } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const rect1 = Rect.one({
+  fill: '#FF4B4B',
+  cornerRadius: 20
+}, 100, 100);
+const rect2 = Rect.one({
+  fill: '#32cd79',
+  cornerRadius: 50
+}, 150, 150);
+leafer.add([rect1, rect2]);
+setTimeout(() => {
+  // 设置混合模式
+  rect2.blendMode = 'multiply'; // [!code hl]
+}, 1000);
