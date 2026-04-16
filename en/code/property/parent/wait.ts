8c793a385f08c50838799a18e0c4acc4ff4daf0a
Leafer <83396016+leaferjs@users.noreply.github.com>
2026-04-16 09:31:47 +0800

    New translations wait.ts (English)

---
+++ en/code/property/parent/wait.ts
@@ -0,0 +1,15 @@
+// #等待元素被添加到父元素中时，执行回调 (Leafer)
+import { Leafer, Group, Rect } from 'leafer-ui';
+const leafer = new Leafer({
+  view: window
+});
+const rect = new Rect({
+  fill: '#32cd79'
+});
+const group = new Group();
+rect.waitParent(() => {
+  // [!code hl:3]
+  rect.draggable = true;
+});
+group.add(rect);
+leafer.add(group);
